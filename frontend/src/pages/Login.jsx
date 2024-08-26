30a2ec97e8bdb54600d4f565c0e9c13f019133a4
satyam-kanaujiya <140954810+satyam-kanaujiya@users.noreply.github.com>
2024-08-26 22:08:49 +0530

    Changed color of submit button in Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -47,7 +47,7 @@ function Login() {
             <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-slate-800">
                 login  
             </a>
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="w-full bg-slate-300 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 
                     <form className="space-y-4 md:space-y-6" onSubmit={loginUser}>
@@ -63,7 +63,8 @@ function Login() {
                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password" value={data.password} onChange={(e)=>setData({...data,password:e.target.value})}placeholder="" autoComplete='off' className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                         </div>
-                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">submit</button>
+                        <button type="submit" className="w-full text-black hover:bg-slate-200 hover:text-black
+                     focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:hover:text-black dark:hover:bg-blue-200">submit</button>
                         <div className="text-sm font-light text-gray-500 dark:text-gray-400">
                             Don't have yet an account?
                             <Link to={"/register"}><span className="font-medium text-primary-600 hover:underline dark:text-primary-500"> register</span></Link>
